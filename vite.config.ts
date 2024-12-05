d280e7ca70315889a931726c6a00f9933f3eae50
andryxatt <vlandroid228@gmail.com>
2024-12-05 10:30:05 +0200

    added changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Andryxatt.github.io",
+  base: "/andryxatt.github.io",
 });
