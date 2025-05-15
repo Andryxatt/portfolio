3e2c7eca4feb77415b91d175696b03a81ea01f09
andryxatt <vlandroid228@gmail.com>
2025-05-16 00:01:21 +0300

    fixed main 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { HashRouter } from 'react-router-dom'; // 👈 зміна тут
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
