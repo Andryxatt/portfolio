faed8afc169d2f81aaf280dfedac14f263ac77ee
andryxatt <vlandroid228@gmail.com>
2025-05-15 23:59:55 +0300

    fixed main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
