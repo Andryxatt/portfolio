169181eb4a4683cfc81cb589cb0baa668240b89a
Andryxatt <vlandroid228@gmail.com>
2025-10-15 20:49:26 +0300

    fixed changes homepage

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // ← це має відповідати репозиторію GitHub: andryxatt.github.io/**portfolio**
+  base: '/portfolio', // ← це має відповідати репозиторію GitHub: andryxatt.github.io/**portfolio**
 });
